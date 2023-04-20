ad339cef660ca3f9e66cb38f9ab66d548450d0d4
Mikita Havryliuk <chinter.games1@gmail.com>
2023-04-20 12:07:53 +0300

    fixed img

--- src/components/Services/ServiceItem/ServiceItem.component.tsx
+++ src/components/Services/ServiceItem/ServiceItem.component.tsx
@@ -12,7 +12,7 @@ export const ServiceItem: React.FC<Props> = ({ service: { description, icon } })
       target="_blank"
       className="services__item"
     >
-        <img src={icon} alt="service-icon" className="services__icon" />
+        <img src={`src/assets/${icon}`} alt="service-icon" className="services__icon" />
         <p className="services__description">{description}</p>
     </a>
   );
