3e5e65254ffd59c727ca1e0877e6089b44dc5c10
Mikita Havryliuk <chinter.games1@gmail.com>
2023-04-20 18:56:48 +0300

    implement benefits

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Services } from "./components/Services";
+import { Benefits } from "./components/Benefits/Benefits.component";
 
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -23,6 +24,7 @@ function App() {
       <Header windowWidth={windowWidth} />
       <main className="main">
         <Services />
+        <Benefits />
       </main>
     </div>
   );
---
+++ src/types/benefits.type.ts
@@ -0,0 +1,5 @@
+export interface Benefit {
+   id: number;
+   icon: string;
+   text: string;
+};
