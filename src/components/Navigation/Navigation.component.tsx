5ef4bbc73b350b5422ded2e6c4f39a5d66badb52
Mikita Havryliuk <chinter.games1@gmail.com>
2023-04-24 00:33:02 +0300

    improve navigation

--- src/components/Navigation/Navigation.component.tsx
+++ src/components/Navigation/Navigation.component.tsx
@@ -5,19 +5,34 @@ export const Navigation = () => {
     <nav className="navigation">
       <ul className="navigation__list">
         <li className="navigation__item">
-          <a href="#services" className="navigation__link">Services</a>
+          <a href="#" className="navigation__link">
+            Home
+          </a>
         </li>
         <li className="navigation__item">
-          <a href="#pricing" className="navigation__link">Pricing</a>
+          <a href="#services" className="navigation__link">
+            Services
+          </a>
         </li>
         <li className="navigation__item">
-          <a href="#cases" className="navigation__link">Cases</a>
+          <a href="#pricing" className="navigation__link">
+            Pricing
+          </a>
         </li>
         <li className="navigation__item">
-          <a href="#about" className="navigation__link">About</a>
+          <a href="#cases" className="navigation__link">
+            Cases
+          </a>
         </li>
         <li className="navigation__item">
-          <a href="#contacts" className="navigation__link">Contacts</a>
+          <a href="#about" className="navigation__link">
+            About
+          </a>
+        </li>
+        <li className="navigation__item">
+          <a href="#contacts" className="navigation__link">
+            Contacts
+          </a>
         </li>
       </ul>
     </nav>
