a3e2af1f411adaf8c31b2a095f0136ad4a7d5788
Mikita Havryliuk <chinter.games1@gmail.com>
2023-04-24 20:48:35 +0300

    added active link

--- src/components/Navigation/Navigation.component.tsx
+++ src/components/Navigation/Navigation.component.tsx
@@ -1,39 +1,59 @@
 import "./Navigation.component.css";
+import { Link } from "./types/link.type";
+import { useCallback, useState } from "react";
 
 export const Navigation = () => {
+  const links: Link[] = [
+    {
+      id: 1,
+      name: "Home",
+      href: "#",
+    },
+    {
+      id: 2,
+      name: "Services",
+      href: "#services",
+    },
+    {
+      id: 3,
+      name: "Pricing",
+      href: "#pricing",
+    },
+    {
+      id: 4,
+      name: "Cases",
+      href: "#cases",
+    },
+    {
+      id: 5,
+      name: "About",
+      href: "#about",
+    },
+    {
+      id: 6,
+      name: "Contacts",
+      href: "#contacts",
+    },
+  ];
+  const [activeLink, setActiveLink] = useState("Home");
+  const handleClick = useCallback((name: string) => setActiveLink(name), []);
+
   return (
     <nav className="navigation">
       <ul className="navigation__list">
-        <li className="navigation__item">
-          <a href="#" className="navigation__link">
-            Home
-          </a>
-        </li>
-        <li className="navigation__item">
-          <a href="#services" className="navigation__link">
-            Services
-          </a>
-        </li>
-        <li className="navigation__item">
-          <a href="#pricing" className="navigation__link">
-            Pricing
-          </a>
-        </li>
-        <li className="navigation__item">
-          <a href="#cases" className="navigation__link">
-            Cases
-          </a>
-        </li>
-        <li className="navigation__item">
-          <a href="#about" className="navigation__link">
-            About
-          </a>
-        </li>
-        <li className="navigation__item">
-          <a href="#contacts" className="navigation__link">
-            Contacts
-          </a>
-        </li>
+        {links.map((link) => (
+          <li className="navigation__item" key={link.id}>
+            <a
+              href={link.href}
+              className={`navigation__link${
+                activeLink === link.name ? " navigation__link--active" : ""
+              }`}
+              onClick={() => handleClick(link.name)}
+            >
+              {link.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
