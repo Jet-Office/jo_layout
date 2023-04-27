10eb3da59b51a9757dca14d9044fafac5c12703c
Mikita Havryliuk <chinter.games1@gmail.com>
2023-04-27 17:23:45 +0300

    some fixes

--- src/components/ControlPreview/ControlPreview.compoent.tsx
+++ src/components/ControlPreview/ControlPreview.compoent.tsx
@@ -2,8 +2,8 @@ import "./ControlPreview.component.css";
 
 export const ControlPreview = () => {
   return (
-    <div className="control">
-      <div className="control__background">
+    <div className="control__background">
+      <div className="control">
         <div className="control__title_container">
           <h2 className="control__title">
             Mission control for your business, you can trust
