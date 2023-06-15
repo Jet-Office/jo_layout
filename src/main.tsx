6766d44d627a1787bfcce96bb02bb73c41ee2121
Mikita Havryliuk <chinter.games1@gmail.com>
2023-06-15 10:14:14 +0300

    fixed paths

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 import "./index.css";
-import { ModalProvider } from "./context/modalProvider";
+import { ModalProvider }  from "./context/modalProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ModalProvider>
