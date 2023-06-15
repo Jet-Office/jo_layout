8fb73472328cc961d70d3e23e79f91076bbe7f01
Mikita Havryliuk <chinter.games1@gmail.com>
2023-06-15 10:28:51 +0300

    provider to app

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,11 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 import "./index.css";
-import { ModalProvider }  from "./context/modalProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ModalProvider>
-    <HashRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </HashRouter>
-  </ModalProvider>
+  <HashRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HashRouter>
 );
