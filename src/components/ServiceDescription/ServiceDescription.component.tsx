e2f25d27ab443fe6af9fb592f05c46ed3d5718c2
Oleksii Pustovit <91pustovit91@gmail.com>
2023-07-26 18:46:43 +0300

    changes json

--- src/components/ServiceDescription/ServiceDescription.component.tsx
+++ src/components/ServiceDescription/ServiceDescription.component.tsx
@@ -4,55 +4,6 @@ import servicesData from "../../data/servicesPage.json";
 
 import "./ServiceDescription.component.css";
 
-// Оголошення типу даних для об'єкта підпослуги (list)
-
-
-interface ListItem {
-  id: number;
-  title: string;
-  icon: string;
-  description: string;
-  link: string;
-}
-
-// Оголошення типу даних для об'єкта послуги
-interface SubService {
-  id: number;
-  "hero-title": string;
-  "hero-description": string;
-  "hero-background": string;
-  list: ListItem[];
-  "main-image": string;
-  "main-title": string;
-  "main-description": string;
-  list2: SubListItem[];
-  "rocket-image"?: string; // Додали "?" для опціонального поля
-  "rocket-title"?: string; // Додали "?" для опціонального поля
-  "rocket-description"?: string; // Додали "?" для опціонального поля
-}
-
-// Оголошення типу даних для об'єкта підпослуги з "list2"
-interface SubListItem {
-  id: number;
-  number: string;
-  title: string;
-  description: string;
-}
-
-// Оголошення типу даних для об'єкта послуги
-interface ServicePageItem {
-  id: number;
-  title: string;
-  description: string;
-  background: string;
-  link: string;
-  list: ListItem[];
-}
-
-// Оголошення типу даних для об'єкта, що містить усі послуги
-interface ServicesPageData {
-  servicesPage: ServicePageItem[];
-}
 
 export const ServiceDescription: React.FC = () => {
   const { link, subLink } = useParams<{ link: string; subLink: string }>();
