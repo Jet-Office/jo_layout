0591c82a8a440971a90390dfbb674e78c740a2e0
xqudee <annamorozenko2004@gmail.com>
2023-09-12 20:02:29 +0300

    Update Cases.component.css

--- src/components/Cases/components/CasesDescriptions/CasesDescriptions.component.tsx
+++ src/components/Cases/components/CasesDescriptions/CasesDescriptions.component.tsx
@@ -8,22 +8,22 @@ type Props = {
 
 export const CasesDescription: React.FC<Props> = ({ descriptions }) => {
   return (
-    <Card className="cases__descriptions">
-      {descriptions.map(({ id, name, text }) => (
-        <div key={id} className="cases__description_wrapper">
-          <p className="cases__card_name">{`${name}:`}</p>
-          <ul className="cases__description_list">
-            {text.map((item) => (
-              <li
-                key={item}
-                className="cases__card_text cases__text_description"
-              >
-                {item}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </Card>
+      <Card className="cases__descriptions">
+        {descriptions.map(({ id, name, text }) => (
+          <div key={id} className="cases__description_wrapper">
+            <p className="cases__card_name">{`${name}:`}</p>
+            <ul className="cases__description_list">
+              {text.map((item) => (
+                <li
+                  key={item}
+                  className="cases__card_text cases__text_description"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Card>
   );
 };
